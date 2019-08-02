2bbd198be8e6b7647c284da5037d449e4326ef36
MikaB01 <mikabessel@googlemail.com>
2019-08-02 13:36:21 +0200

    Add and Edit Frame layout Bug fixed

--- app/assets/javascripts/frame.js
+++ app/assets/javascripts/frame.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     let max_index = $('#max_super_index').attr('data-super-length');
 
     //Add Frame
-    for (let i = 0; i < max_index; i++) {
+    for (let i = 0; i <= max_index; i++) {
 
         if($("#super_id_" + i).attr('data-super-id'))
             document.getElementById('hidden_super_id_' + i).value = $("#super_id_" + i).attr('data-super-id');
@@ -17,6 +17,7 @@ $(document).ready(function() {
         });
 
         $('#new_frame_heading_' + i).unbind("click").click(function () {
+            $('#close_add_frame_' + i).css("color", "#777");
             document.getElementById('img_new_frame_' + i).classList.add('hidden');
             document.getElementById('new_frame_heading_' + i).classList.add('hidden');
             $('.new_frame_form').css("width", "320px");
@@ -26,42 +27,82 @@ $(document).ready(function() {
         $('#close_add_frame_' + i).unbind("click").click(function () {
             switch_active_class(document.getElementById('new_frame_form_' + i), 'show_form', 'hide');
             $('.new_frame').css("width", "90px");
+            document.getElementById('new_frame_form_' + i).classList.add('hidden');
             document.getElementById('img_new_frame_' + i).classList.remove('hidden');
             document.getElementById('new_frame_heading_' + i).classList.remove('hidden');
         });
 
+        //Add Frame close_add :hover -color
+        $(document).on("mouseenter", '#new_frame_form_' + i, function () {
+            $('#close_add_frame_' + i).css("color", "#777");
+        });
+
+        $(document).on("mouseenter", '#close_add_frame_' + i, function () {
+            $('#close_add_frame_' + i).css("color", "#f2d002");
+        });
+
+        $(document).on("mouseleave", '#new_frame_form_' + i, function () {
+            $('#close_add_frame_' + i).css("color", "transparent");
+        });
+
+        $(document).on("mouseleave", '#close_add_frame_' + i, function () {
+            $('#close_add_frame_' + i).css("color", "#777");
+        });
+
         let frame_amount = $('#frame_amount' + i).attr('data-frame_amount');
-        console.log(frame_amount + "  " + max_index + "  " + i);
         for (let f = 0; f < frame_amount; f++) {
             $('#edit_frame_icon_' + i + '_' + f).unbind("click").click(function () {
-                $('.edit_frame_form').css("width", "320px");
-                switch_active_class(document.getElementById('edit_frame_form_' + i + '_' + f), 'show_form', 'hide');
+                if(document.getElementById('new_frame_form_' + i).classList.contains('hide') &&
+                        (
+                            document.getElementById('frame_amount' + i).classList.contains('close') ||
+                            document.getElementById('edit_frame_form_' + i + '_' + f).classList.contains('show_form'))) {
+                    $('#close_edit_frame_' + i + '_' + f).css("color", "#777");
+                    document.getElementById('img_new_frame_' + i).classList.add('hidden');
+                    document.getElementById('new_frame_heading_' + i).classList.add('hidden');
+                    $('#edit_frame_form_' + i + '_' + f).css("width", "320px");
+                    document.getElementById('frame_amount' + i).classList.remove('close');
+                    document.getElementById('frame_amount' + i).classList.add('open');
+                    switch_active_class(document.getElementById('edit_frame_form_' + i + '_' + f), 'show_form', 'hide');
+                    if(document.getElementById('edit_frame_form_' + i + '_' + f).classList.contains('hide')) {
+                        $('#edit_frame_form_' + i + '_' + f).css("width", "90px");
+                        document.getElementById('edit_frame_form_' + i + '_' + f).classList.add('hidden');
+                        document.getElementById('img_new_frame_' + i).classList.remove('hidden');
+                        document.getElementById('new_frame_heading_' + i).classList.remove('hidden');
+                        document.getElementById('frame_amount' + i).classList.remove('open');
+                        document.getElementById('frame_amount' + i).classList.add('close');
+                    }
+                }
             });
 
             $('#close_edit_frame_' + i + '_' + f).unbind("click").click(function () {
                 switch_active_class(document.getElementById('edit_frame_form_' + i + '_' + f), 'show_form', 'hide');
-                $('.edit_frame_form').css("width", "90px");
+                $('#edit_frame_form_' + i + '_' + f).css("width", "90px");
+                document.getElementById('edit_frame_form_' + i + '_' + f).classList.add('hidden');
+                document.getElementById('img_new_frame_' + i).classList.remove('hidden');
+                document.getElementById('new_frame_heading_' + i).classList.remove('hidden');
+                document.getElementById('frame_amount' + i).classList.remove('open');
+                document.getElementById('frame_amount' + i).classList.add('close');
+            });
+
+            //Edit Frame close_edit :hover -color
+            $(document).on("mouseenter", '#edit_frame_form_' + i + '_' + f, function () {
+                $('#close_edit_frame_' + i + '_' + f).css("color", "#777");
+            });
+
+            $(document).on("mouseenter", '#close_edit_frame_' + i + '_' + f, function () {
+                $('#close_edit_frame_' + i + '_' + f).css("color", "#f2d002");
+            });
+
+            $(document).on("mouseleave", '#edit_frame_form_' + i + '_' + f, function () {
+                $('#close_edit_frame_' + i + '_' + f).css("color", "transparent");
+            });
+
+            $(document).on("mouseleave", '#close_edit_frame_' + i + '_' + f, function () {
+                $('#close_edit_frame_' + i + '_' + f).css("color", "#777");
             });
         }
 
     }
-
-    //Add Frame
-    $(document).on("mouseenter", '.new_frame_form', function () {
-        $('.close_add_frame').css("color", "#34322c");
-    });
-
-    $(document).on("mouseenter", '.close_add_frame', function () {
-        $('.close_add_frame').css("color", "#f2d002");
-    });
-
-    $(document).on("mouseleave", '.new_frame_form', function () {
-        $('.close_add_frame').css("color", "transparent");
-    });
-
-    $(document).on("mouseleave", '.close_add_frame', function () {
-        $('.close_add_frame').css("color", "#34322c");
-    });
 
     function switch_active_class(element, class_name1, class_name2) {
         if (hasClass(element, 'hidden')) element.classList.remove('hidden');
